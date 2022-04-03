44ad28015f2cd60c1f42195dd0b07ce1c31a226f
yuvakiran1808 <pyuvasaikiran@gmail.com>
2022-04-03 19:39:13 +0530

    bug fixed

--- calcscript.js
+++ calcscript.js
@@ -1,26 +1,26 @@
-const values =  document.getElementById("content");
-function dis(val){
+const values = document.getElementById("content");
+function dis(val) {
 
-    values.value+=val;
-    
- }
-    
-  function solve()
-    {
-    let  key = document.getElementById("content").value;
-    if(isNaN(key))
-    {
-        document.getElementById("content").value = "ENTER VALID INPUT";
-        setTimeout(()=>{
-        document.getElementById("content").value = "";
+    values.value += val;
 
-        },1000);
-    }
-// let x = document.getElementById("content").value
-let y = eval(key)
-document.getElementById("content").value = y
 }
-function clr()
-{
-document.getElementById("content").value = ""
+
+function solve() {
+    let key = document.getElementById("content").value;
+    if (isNaN(key)) {
+        document.getElementById("content").value = "ENTER VALID INPUT";
+        setTimeout(() => {
+            if (document.getElementById("content").value == "ENTER VALID INPUT") {
+
+                clr();
+            }
+        }, 1500);
+    }
+    // let x = document.getElementById("content").value
+    let y = eval(key)
+    document.getElementById("content").value = y
+}
+function clr() {
+
+    document.getElementById("content").value = ""
 }
